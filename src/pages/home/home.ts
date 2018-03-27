796ce54a29450e23d19f98ac1196045d2b003cb3
Eduardo <ibredu@gmail.com>
2018-03-27 16:16:50 -0500

    Added dynamic list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {PlacePage} from "../place/place";
+import {PlacesService} from "../../services/places.service";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  notas: any = [
-    {titulo:'Título 1', cuerpo: 'Cuerpo 1', categoria: 'Categoría 1'},
-    {titulo:'Título 2', cuerpo: 'Cuerpo 2', categoria: 'Categoría 2'},
-    {titulo:'Título 3', cuerpo: 'Cuerpo 3', categoria: 'Categoría 3'},
-    {titulo:'Título 4', cuerpo: 'Cuerpo 4', categoria: 'Categoría 4'},
-    {titulo:'Título 5', cuerpo: 'Cuerpo 5', categoria: 'Categoría 5'}
-  ];
-  constructor(public navCtrl: NavController) {
-
+  lugares: any = [];
+  constructor(public navCtrl: NavController, private placesService: PlacesService) {
+    this.placesService.getPlaces().valueChanges()
+      .subscribe((places)=>{
+        this.lugares = places;
+        console.log(this.lugares);
+      });
   }
   irALugar(placeName){
     this.navCtrl.push(PlacePage, {name: placeName});
