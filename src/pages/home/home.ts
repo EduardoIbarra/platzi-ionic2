70962d0c48ef5b09fa3eaca472430ca1efea0553
Eduardo <ibredu@gmail.com>
2018-03-27 09:01:12 -0500

    Added static notes to list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,13 @@ import {PlacePage} from "../place/place";
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  notas: any = [
+    {titulo:'Título 1', cuerpo: 'Cuerpo 1', categoria: 'Categoría 1'},
+    {titulo:'Título 2', cuerpo: 'Cuerpo 2', categoria: 'Categoría 2'},
+    {titulo:'Título 3', cuerpo: 'Cuerpo 3', categoria: 'Categoría 3'},
+    {titulo:'Título 4', cuerpo: 'Cuerpo 4', categoria: 'Categoría 4'},
+    {titulo:'Título 5', cuerpo: 'Cuerpo 5', categoria: 'Categoría 5'}
+  ];
   constructor(public navCtrl: NavController) {
 
   }
